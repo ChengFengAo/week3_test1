df3fc7d6947ccfc1bb882654066d181332786ca7
ChengFeng AO <414356022@qq.com>
2018-04-11 20:14:24 +0800

    InverseReduce.java

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -2,20 +2,32 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        int tmp =this.random.nextInt(3);
+        result=Stream.iterate(number,x->x-tmp)
+                .skip(1)
+                .limit(number/2)
+                .collect(toList());
+        return result;
+
     }
 }
