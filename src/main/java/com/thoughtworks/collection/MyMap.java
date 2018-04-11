130b8c05604f54a232f63e8e5644a8c548bfb0bc
ChengFeng AO <414356022@qq.com>
2018-04-11 21:53:17 +0800

    MyMap.java

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,8 +2,12 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 public class MyMap {
 
@@ -17,22 +21,70 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+       /* for (int i = 0; i < this.array.size(); i++) {
+            this.array.set(i,this.array.get(i)*3);
+        }
+        return this.array;*/
+        List<Integer> result = new ArrayList<>();
+        result = this.array.stream()
+                .map(x -> x * 3)
+                .collect(toList());
+        return result;
+
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        List<String> result = new ArrayList<>();
+       result=this.array.stream()
+               .map( x->((char)(x+64)+ "").toLowerCase())
+               .collect(toList());
+       return result;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+    List<String> result= new ArrayList<>();
+    for (Integer it:array){
+        int tmp=it.intValue();
+        String map =new String();
+        double flags= Math.log(tmp)/Math.log(26);
+        for (int i=0;i<=flags;i++)
+        {
+            char c;
+            int cur;
+            cur=tmp-i*26;
+            int d=cur/26;
+            int m=cur%26;
+            if (m==0){
+                d-=1;
+                m=26;
+            }
+            if(d==0)
+            {
+                cur=m+96;
+                c=(char)cur;
+                map += c;
+            }
+            else {
+                cur=d+96;
+                c=(char)cur;
+                map +=c;
+            }
+
+        }
+        result.add(map);
+    }
+    return result;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        result=array.stream().sorted(Comparator.reverseOrder()).collect(toList());
+        return result;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        result=array.stream().sorted(Comparator.naturalOrder()).collect(toList());
+        return result;
     }
 }
